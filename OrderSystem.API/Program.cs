0e7b3f872406bbf27f9e17212f868e34eaa4ad09
Metin ALTINISIK <metin.altinisik@atlastek.com>
2025-08-02 16:45:04 +0300

    configration file added

--- OrderSystem.API/Program.cs
+++ OrderSystem.API/Program.cs
@@ -3,6 +3,7 @@
 //Console.WriteLine("Hello, World!");
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using OrderSystem.Application.Common.Events;
 using OrderSystem.Application.UseCases;
@@ -12,14 +13,18 @@ using OrderSystem.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://*:5000");
+
 builder.Services.AddSingleton<IOrderRepository, InMemoryOrderRepository>();
 builder.Services.AddScoped<CreateOrder>();
 
 builder.Services.AddControllers();
 
 builder.Services.AddSingleton<IKafkaEventPublisher>(
- new KafkaEventPublisher("localhost:9092")
+ new KafkaEventPublisher("kafka:9092")
 );
+
+//builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
 var app = builder.Build();
 
